6d97dff3669d20fbddc67d0bd52f8f54887f3d02
bdo6612 <sobczyk.bruno@gmail.com>
2022-12-01 14:36:56 +0100

    cos zrobilem

--- main.py
+++ main.py
@@ -1,24 +1,25 @@
 
 
 dict_tt = {
-   1 : '14:30',
-   2 : '15:30',
-   3 : '16:30'
+   0: '14:30',
+   1: '15:30',
+   2: '16:30'
 }
 class Teacher:
     def __init__(self, timetable, name):
        self.timetable = [] ## 0 - to be occupied, 1- not avalible, <string> - occupied
        for i in enumerate(dict_tt):
-          if i + 1 in timetable:
+          if i in timetable:
+             self.timetable.append(0)
+          else :
              self.timetable.append(1)
 
+
        self.name = name
-       self.occupied = []
+    def occupie(self, parent, hour):
+      self.timetable[hour] = parent
+
+nauczyciel1 = Teacher([1,2], 'imie1')
+nauczyciel2 = Teacher([0, 2], 'imie2')
 
 
-   def occupation_list(self):
-print("d")
-
-
-
-nauczyciel1 = Teacher([1,3], imie1)
