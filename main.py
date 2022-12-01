06d2844066219245a126c76a471b45691ab3c25b
bdo6612 <sobczyk.bruno@gmail.com>
2022-12-01 13:55:16 +0100

    s

---
+++ main.py
@@ -0,0 +1,23 @@
+
+
+dict_tt = {
+   1 : '14:30',
+   2 : '15:30',
+   3 : '16:30'
+}
+class Teacher:
+    def __init__(self, timetable, name):
+       self.timetable = [] ## 0 - to be occupied, 1- not avalible, <string> - occupied
+       for i in enumerate(dict_tt):
+          if i + 1 in timetable:
+             self.timetable.append(1)
+
+       self.name = name
+       self.occupied = []
+
+
+   def occupation_list(self):
+
+
+
+nauczyciel1 = Teacher([1,3], imie1)
